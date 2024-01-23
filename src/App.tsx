3983c1172642eba2088c0422dcce826435e86564
Jahkamso <uzoekwejahkamso@gmail.com>
2024-01-23 23:03:34 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css"
+
+interface Image {
+  url: string
+}
+
+export default function App() {
+  const [data, setData] = useState<Image[]>([])
+
+  const envFile = import.meta.env; // access .env file
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`${envFile.VITE_IMAGES_URL}`)
+      console.log(setData(res.data))
+    }
+    fetchData()
+
+  }, [])
+  return (
+    <div>
+      <h1>Images</h1>
+      {data.map(image => (
+        <img key={image.url} src={`${image.url}`} style={{width: "200px"}} alt="" />
+      ))}
+    </div>
+  )
+}
